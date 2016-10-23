53a339ea325de9beb31eac3d59b72d8ace6e23d7
alegeperformanta <office.alegeperformanta@gmail.com>
2016-10-22 19:13:00 -0700

    +Update dummy .css file and routing
    -dummy .css file from startbootsrap
    -routing for components

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+//My components
+import {HomeComponent} from './components/home/home.component';
+import {ProfileComponent} from './components/profile/profile.component';
+//End my components
+
+//My routing
+import {routing, appRoutingProviders} from './app.routing';
+//End my routing
+
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [ BrowserModule, routing ],
+  declarations: [ AppComponent, HomeComponent, ProfileComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [appRoutingProviders]
 })
 export class AppModule { }
